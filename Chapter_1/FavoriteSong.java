35fd171c95d1e7120ba5bd75c56bd89d29b5ebad
JaphetPaulTYburlas-UCM <japhetpaultrimidalyburlas@gmail.com>
2025-09-02 17:49:27 -0700

    Create FavoriteSong.java

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,16 @@
+public class Favorite {
+    public static void main(String[] args) {
+          System.out.print("Mabuti pa sa lotto, may pag-asang manalo\r\n" + //
+                        "'Di tulad sa'yo, imposible");
+        System.out.println("Prinsesa ka, ako'y dukha\r\n" + //
+                        "Sa TV lang naman kasi may mangyayari");
+        System.out.println("At kahit mahal kita\r\n" + //
+                        "Wala akong magagawa\r\n" + //
+                        "Tanggap ko, oh, aking sinta\r\n" + //
+                        "Pangarap lang kita");
+        System.out.println("Ang hirap maging babae kung torpe 'yung lalaki\r\n" + //
+                        "Kahit may gusto ka, 'di mo masabi\r\n" + //
+                        "Hindi ako 'yung tipo, nagbibigay motibo\r\n" + //
+                        "Conservative ako kaya 'di maaari");   
+    }
+}

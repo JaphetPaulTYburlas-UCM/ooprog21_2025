02bc9d60aa5cd35fd45ba68cfd166f38552bda21
JaphetPaulTYburlas-UCM <japhetpaultrimidalyburlas@gmail.com>
2025-09-14 18:13:27 -0700

    Create CompanyInfo.java

---
+++ CHAPTER 3/CompanyInfo.java
@@ -0,0 +1,15 @@
+
+public class CompanyInfo {
+    public static void main(String[] args) {
+        Electronics();
+        displaysHours();
+    }
+     static void Electronics(){
+       System.out.println("Smart Electronics");
+    }
+    static void displaysHours(){
+        System.out.println("Monday - Friday 8 am to 6 pm");
+        System.out.println("Saturday 8 am to noon");
+        System.out.println("Sunday Closed");        
+    }
+}

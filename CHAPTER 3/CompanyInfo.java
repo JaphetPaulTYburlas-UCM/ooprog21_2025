df61504c3a9cb3f83e98cb810c2a8c14f1782998
JaphetPaulTYburlas-UCM <japhetpaultrimidalyburlas@gmail.com>
2025-09-15 23:24:37 -0700

    Update CompanyInfo.java

--- CHAPTER 3/CompanyInfo.java
+++ CHAPTER 3/CompanyInfo.java
@@ -1,15 +1,15 @@
 
 public class CompanyInfo {
     public static void main(String[] args) {
-        Electronics();
+        displayCompanyName();
         displaysHours();
     }
-     static void Electronics(){
+     static void displayCompanyName(){
        System.out.println("Smart Electronics");
     }
     static void displaysHours(){
-        System.out.println("Monday - Friday 8 am to 6 pm");
-        System.out.println("Saturday 8 am to noon");
-        System.out.println("Sunday Closed");        
-    }
+        System.out.println("Monday - Friday 8 am - 6m"
+                            +"Satuday       8 am - noon\n"
+                            +"Sunday         Closed");
+    }       
 }

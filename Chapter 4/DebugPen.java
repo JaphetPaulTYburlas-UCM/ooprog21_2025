9a956af8d237a47c1c6ff479dc957e18bc3e7ace
JaphetPaulTYburlas-UCM <japhetpaultrimidalyburlas@gmail.com>
2025-09-23 19:02:51 -0700

    Update DebugPen.java

--- Chapter 4/DebugPen.java
+++ Chapter 4/DebugPen.java
@@ -1,37 +1,23 @@
+
 public class DebugPen {
     private String color;
     private String point;
 
-    // Default constructor
     public DebugPen() {
         color = "black";
         point = "fine";
     }
 
-    // Parameterized constructor
     public DebugPen(String color, String point) {
-        this.color = color;
-        this.point = point;
+        color = color;
+        point = point;
     }
 
-    // Getter for color
     public String getColor() {
         return color;
     }
 
-    // Getter for point
     public String getPoint() {
         return point;
-    }
-
-    // Main method to test the class
-    public static void main(String[] args) {
-        // Create pen using default constructor
-        DebugPen pen1 = new DebugPen();
-        System.out.println("Pen1 - Color: " + pen1.getColor() + ", Point: " + pen1.getPoint());
-
-        // Create pen using parameterized constructor
-        DebugPen pen2 = new DebugPen("blue", "medium");
-        System.out.println("Pen2 - Color: " + pen2.getColor() + ", Point: " + pen2.getPoint());
     }
 }

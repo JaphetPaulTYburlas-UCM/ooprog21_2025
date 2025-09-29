e32566e90c6d29e45cc1282f37c30ef26fff011e
JaphetPaulTYburlas-UCM <japhetpaultrimidalyburlas@gmail.com>
2025-09-28 17:38:13 -0700

    Create DebugFour3.java

---
+++ Chapter 4/DebugFour3.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class DebugFour3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int w, l, h;
+
+        System.out.print("Enter width of box >> ");
+        w = input.nextInt();
+
+        System.out.print("Enter length of box >> ");
+        l = input.nextInt();
+
+        System.out.print("Enter height of box >> ");
+        h = input.nextInt();
+
+        // Create two boxes
+        DebugBox box1 = new DebugBox();           // default
+        DebugBox box2 = new DebugBox(w, l, h);    // user values
+
+        System.out.println("The dimensions of the first box are");
+        box1.showData();
+        System.out.print(" The volume of the first box is ");
+        showVolume(box1);
+
+        System.out.println("The dimensions of the second box are");
+        box2.showData();
+        System.out.print(" The volume of the second box is ");
+        showVolume(box2);
+    }
+
+    public static void showVolume(DebugBox aBox) {
+        double vol = aBox.getVolume();
+        System.out.println(vol);
+    }
+}
+
+// DebugBox class in same file
+class DebugBox {
+    private int width;
+    private int length;
+    private int height;
+
+    // Default constructor
+    public DebugBox() {
+        width = 1;
+        length = 1;
+        height = 1;
+    }
+
+    // Constructor with args
+    public DebugBox(int w, int l, int h) {
+        width = w;
+        length = l;
+        height = h;
+    }
+
+    public void showData() {
+        System.out.println(" Width: " + width);
+        System.out.println(" Length: " + length);
+        System.out.println(" Height: " + height);
+    }
+
+    public double getVolume() {
+        return width * length * height;
+    }
+}

e6125d0039692787b8bd15fe374c32ef1c75a4c1
JaphetPaulTYburlas-UCM <japhetpaultrimidalyburlas@gmail.com>
2025-11-02 06:10:38 -0800

    Create DebugSix2.java

---
+++ Chapter_6/DebugSix2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class DebugSix2
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        char letter;
+        int a;
+        int min;  // not final, since we assign later
+        final int MAX = 122;
+        final int NUMBERLINE = 20;
+        int lines = 0;
+
+        System.out.println("Enter a Unicode value to start.");
+        System.out.print("For example, A is 65 >> ");
+        min = keyboard.nextInt();
+
+        for (a = min; a <= MAX; a++)
+        {
+            letter = (char) a;
+            System.out.print(" " + letter);
+            lines++;
+
+            if (lines == NUMBERLINE)
+            {
+                System.out.println();
+                lines = 0;
+            }
+        }
+
+        System.out.println("\nEnd of application");
+        keyboard.close();
+    }
+}

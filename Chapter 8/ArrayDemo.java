c72856aa447a91e6237edcd55b9ce7b44cc86db3
JaphetPaulTYburlas-UCM <japhetpaultrimidalyburlas@gmail.com>
2025-12-03 04:15:35 -0800

    Update ArrayDemo.java

--- Chapter 8/ArrayDemo.java
+++ Chapter 8/ArrayDemo.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
+
 public class ArraysDemo {
 
     public static void display(String message, int[] array) {
-        System.out.println(message);
+        System.out.print(message + "    ");
         for (int value : array) {
             System.out.print(value + " ");
         }
@@ -9,5 +11,19 @@ public class ArraysDemo {
     }
 
     public static void main(String[] args) {
+
+        int[] numbers = new int[4];
+
+        display("Original array:", numbers);
+
+        Arrays.fill(numbers, 8);
+        display("After filling with 8s:", numbers);
+
+        numbers[0] = 3;
+        numbers[2] = 6;
+        display("After changing two values:", numbers);
+
+        Arrays.sort(numbers);
+        display("After sorting:", numbers);
     }
 }

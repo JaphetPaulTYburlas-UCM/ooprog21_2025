9135f17ad123847f0eaf6fa4460fb252d7557b70
JaphetPaulTYburlas-UCM <japhetpaultrimidalyburlas@gmail.com>
2025-12-03 04:12:20 -0800

    Create ArrayDemo.java

---
+++ Chapter 8/ArrayDemo.java
@@ -0,0 +1,13 @@
+public class ArraysDemo {
+
+    public static void display(String message, int[] array) {
+        System.out.println(message);
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+    }
+}
